00b54a6a27c23705b4e2476b71593165865c963b
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-04-26 07:13:03 +0530

    User actions config

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,12 +1,17 @@
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import {
-  LeftSideBar,
-  MainContainer,
-  TopBar,
-  RightSideBar,
-  BottomBar,
-} from "@/components/layout/index.js";
+// import {
+//   LeftSideBar,
+//   MainContainer,
+//   TopBar,
+//   RightSideBar,
+//   BottomBar,
+// } from "@/components/layout/index.js";
+import LeftSideBar from "../../components/layout/LeftSideBar"
+import MainContainer from "../../components/layout/MainContainer"
+import TopBar from "../../components/layout/TopBar"
+import RightSideBar from "../../components/layout/RightSideBar"
+import BottomBar from "../../components/layout/BottomBar"
 import "../globals.css";
 
 export const metadata = {
--- components/layout/BottomBar.jsx
+++ components/layout/BottomBar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { sidebarLinks } from "@/constants";
+import { sidebarLinks } from "@/constants/index.js";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
